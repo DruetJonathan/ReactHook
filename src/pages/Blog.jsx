b9cf20bf2b8e9561e87dc3c4ed28de9516d6c8ef
Jonathan Druet <jonathandruet2@gmail.com>
2023-10-18 09:23:38 +0200

    animation

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,13 +1,38 @@
-import {NavLink, useLoaderData} from "react-router-dom";
+import { NavLink, useLoaderData } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useToggle } from "./useToggle.js";
+import { forwardRef } from "react";
 
 export function Blog() {
-    const posts = useLoaderData()
-    return <>
-        <h1>Mon blog</h1>
-        <ul>
-            {posts.map(post => <li key={post.id}>
-                <NavLink to={post.id}>{post.title}</NavLink>
-            </li>)}
-        </ul>
-    </>
+    const posts = useLoaderData();
+    const boxVariants = {
+        visible: { x: 0, rotate: 0 },
+        hidden: { x: 100, rotate: 45 },
+
+    };
+    const wrapperVariants = {
+        visible: {opacity: 1 ,transition:{when:'beforeChildren',staggerChildren:.2}},
+        hidden: {opacity: 0,transition:{when:'afterChildren',staggerChildren:.2}},
+    }
+    const [open, toggle] = useToggle(true);
+
+    return (
+        <>
+            <h1>Mon blog</h1>
+            <motion.div animate={open ? 'visible' : 'hidden'}
+                variants={wrapperVariants}
+            >
+                <MotionBox variants={boxVariants}>1</MotionBox>
+                <MotionBox variants={boxVariants}>2</MotionBox>
+                <MotionBox variants={boxVariants}>3</MotionBox>
+            </motion.div>
+            <button onClick={toggle}>Changer état</button>
+        </>
+    );
 }
+
+const Box = forwardRef(({ children }, ref) => {
+    return <div className="box" ref={ref}>{children}</div>;
+});
+
+const MotionBox = motion(Box);
